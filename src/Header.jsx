58380ff052c454d94f76feb9f00949cb146ad9e2
Omid Shahini <moshahini@yahoo.com>
2025-09-05 16:29:23 +0330

    fav and styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
             <a href="#">
               <img src="src/assets/images/header-logo.svg" alt="nike logo" />
             </a>
-            <ul className='flex flex-row justify-between items-center gap-20 text-zinc-600'>
+            <ul className='w-xl h-10 p-3 flex flex-row justify-between items-center gap-20 text-zinc-600 [&>li]:hover:cursor-pointer [&>li]:hover:text-white'>
               <li>Home</li>
               <li>About Us</li>
               <li>Products</li>
               <li>Contacu Us</li>
             </ul>
-            <div className='text-white text-xl border-2 border-amber-100 px-1.5 border-r-amber-800 text-center rounded-se-2xl rounded-es-2xl border-l-amber-800'>
+            <div className='text-white text-xl p-1 border-2 border-amber-100 px-1.5 border-r-lime-200 text-center rounded-se-2xl rounded-es-2xl border-l-amber-800'>
               <a href="#">Sign in</a>
               <span>/</span>
               <a href="#">Explore Now</a>
