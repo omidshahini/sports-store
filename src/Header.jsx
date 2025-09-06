38c75954aed5b4fadffc4e9589c9d2510bd1b1b1
Omid Shahini <moshahini@yahoo.com>
2025-09-06 20:26:15 +0330

    review

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,16 +11,16 @@ const Header = () => {
             <a href="#">
               <img src="src/assets/images/header-logo.svg" alt="nike logo" />
             </a>
-            <ul className='w-xl h-10 p-3 flex flex-row justify-between items-center gap-20 text-zinc-600 [&>li]:hover:cursor-pointer [&>li]:hover:text-white'>
+            <ul className='header-ul'>
               <li>Home</li>
               <li>About Us</li>
               <li>Products</li>
               <li>Contacu Us</li>
             </ul>
-            <div className='text-white text-xl p-1 border-2 border-amber-100 px-1.5 border-r-lime-200 text-center rounded-se-2xl rounded-es-2xl border-l-amber-800'>
-              <a href="#">Sign in</a>
+            <div className='login'>
+              <a href="#">Sign in </a>
               <span>/</span>
-              <a href="#">Explore Now</a>
+              <a href="#"> Explore Now</a>
             </div>
         </navbar>
         </header>
